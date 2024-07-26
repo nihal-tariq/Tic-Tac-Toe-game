9c79df7c65ef9cf0272589b5d1b4c2d3a3edce73
Nihal Tariq <nihaltariq55@gmail.com>
2024-07-27 00:22:31 +0500

    Update tictactoe.jsx
    adding functionality to the winning title of game

--- src/components/tictactoe.jsx
+++ src/components/tictactoe.jsx
@@ -29,39 +29,46 @@ const TicTacToe = ()=>{
 
      const checkWin = ()=>{
          if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
-             won(data);
+             won(data[2]);
          }
          else if(data[3]===data[4] && data[4]===data[5] && data[5]!==""){
-             won(data);
+             won(data[5]);
          }
          else if(data[6]===data[7] && data[7]===data[8] && data[8]!==""){
-             won(data);
+             won(data[8]);
          }
          else if(data[0]===data[3] && data[3]===data[6] && data[6]!==""){
-             won(data);
+             won(data[6]);
          }
          else if(data[1]===data[4] && data[4]===data[7] && data[7]!==""){
-             won(data);
+             won(data[7]);
          }
          else if(data[2]===data[5] && data[5]===data[8] && data[8]!==""){
-             won(data);
+             won(data[8]);
          }
          else if(data[0]===data[4] && data[4]===data[8] && data[8]!==""){
-             won(data);
+             won(data[8]);
          }
          else if(data[2]===data[4] && data[4]===data[6] && data[6]!==""){
-             won(data);
+             won(data[6]);
          }
 
      }
      const won = (winner)=>{
          setLock(true);
+
+         if(winner==="x"){
+             titleRef.current.innerHTML = `Congratulations: <img style="height: 40px" style="padding: 20px 20px"  src="${cross_icon}"  /> wins`;
+         }
+         else if(winner==="o"){
+             titleRef.current.innerHTML = `Congratulations: <img style="height: 40px" style="padding: 20px 20px" src="${circle_icon}"  /> wins`;
+         }
      }
 
 
     return(<div className="h-screen flex flex-col justify-center items-center bg-cyan-950 gap-8">
         <div className=" flex flex-col text-center">
-            <h1 className="text-white mt-0 mb-6 text-3xl flex items-center justify-center">Tic Tac Toe</h1>
+            <h1 className="text-white mt-0 mb-6 text-3xl flex items-center justify-center" ref={titleRef}>Tic Tac Toe</h1>
             <div className="m-auto flex h-min w-min">
                 <div className="row1">
                     <div className="Box flex h-24 w-24 bg-cyan-700 rounded border-gray-800 border-4 hover:bg-cyan-500 cursor-pointer" onClick={(e)=>{toggle(e,0)}}></div>
